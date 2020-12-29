d352c300af7cf7d40061cbcf51893ce42ec7de80
jasper <jasperzhou1129@gmail.com>
2020-12-29 16:10:51 -0500

    setup

---
+++ Food Discovery/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Discovery
+//
+//  Created by Jinglun Zhou on 2020/12/29.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
